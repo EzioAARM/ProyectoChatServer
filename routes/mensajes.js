60e4960819970f259d58ef22b7df67097341d763
jroma96 <31935178+jroma96@users.noreply.github.com>
2018-11-17 20:49:08 -0600

    mensajes.js
    cambios a las rutas

--- routes/mensajes.js
+++ routes/mensajes.js
@@ -4,11 +4,14 @@ const MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 const assert = require('assert');
 
+const url = 'mongodb+srv://roma:1A2basdf@chatdb-53u3w.mongodb.net/test?retryWrites=true';
+const dbName = "ChatProject";
+
 /* GET home page. */
 router.get('/:emisor', function(req, res, next) {
 var emisor = req.params.emisor;
   MongoClient.connect(url, function(err, client) {
-    var collection = cliente.db(dbName).collection("usuarios");
+    var collection = client.db(dbName).collection("usuarios");
     collection.findOne({emisor:emisor}, function(err, documento){
       if (err)
         res.send(404);
@@ -27,7 +30,7 @@ router.get('/:emisor/:receptor', function(req, res, next) {
   var emisor = req.params.emisor;
   var receptor = req.params.receptor;
   MongoClient.connect(url, function(err, client) {
-    var collection = cliente.db(dbName).collection("usuarios");
+    var collection = client.db(dbName).collection("usuarios");
     collection.findOne({emisor:emisor, receptor:receptor}, function(err, documento){
       if (err)
         res.send(404);
@@ -44,23 +47,39 @@ router.get('/:emisor/:receptor', function(req, res, next) {
 
 router.post('/:emisor', function(req, res, next) {
   var json = {
-    "emisor" : req.params.emisor,
-    "receptor" : req.body.receptor,
-    "mensaje" : req.body.mensaje,
-    "archivo" : req.body.archivo
+    emisor : req.params.emisor,
+    receptor : req.body.receptor,
+    mensaje : req.body.mensaje,
+    archivo : req.body.archivo
   };
   MongoClient.connect(url, function(err, client) {
-    var collection = cliente.db(dbName).collection("usuarios");
-    collection.insertOne(json, function(err, documento){
-      if (err)
-        res.send(404);
-      else {
+    var collection = client.db(dbName).collection("usuarios");
+    collection.findOne({username: req.body.receptor}, function(error, result) {
+      if (error) res.send({status: 502});
+      if (!result){
         res.send({
-          data: documento,
-          status: 200
+          status: 404
+        });
+      } else {
+        collection.insertOne({
+          "emisor" : req.params.emisor,
+          "receptor" : req.body.receptor,
+          "mensaje" : req.body.mensaje,
+          "archivo" : req.body.archivo
+        }, function(err, documento){
+          if (err){
+            res.send(404);
+            console.log(err);
+          }
+          else {
+            res.send({
+              status: 200
+            });
+          }
         });
       }
     });
+    
     client.close();
   });
 });
